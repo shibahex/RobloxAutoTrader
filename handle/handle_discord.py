e8010d076a72cc85dac16cbab0924c6fc2a4f3e3
Brandon <80365319+Flaried@users.noreply.github.com>
2024-10-02 02:47:48 -0400

    Add class discord

---
+++ handle/handle_discord.py
@@ -0,0 +1,17 @@
+class Discord():
+    def __init__(self):
+        pass
+
+
+    def post_message(self):
+        pass
+
+    def scan_channel(self):
+        pass
+
+    def scan_value_requests(self):
+        pass
+    
+    def scan_value_albums(self):
+        pass
+
