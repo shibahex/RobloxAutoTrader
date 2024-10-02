7610e5b62a4e7c9705b513395c8ddb2373d88b5e
Brandon <80365319+Flaried@users.noreply.github.com>
2024-10-02 02:39:24 -0400

    Changed discord handler name

---
+++ handle/handle_discord.py
@@ -0,0 +1,17 @@
+
+class Discord():
+    def __init__(self):
+        pass
+
+    def scan_value_requests(self):
+        pass
+
+    def scan_value_albums(self):
+        pass
+
+    def post_trade_advertisement(self):
+        pass
+
+    def post_value_requests(self):
+        pass
+        
