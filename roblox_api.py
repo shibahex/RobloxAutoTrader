bcc9b206bdd43403d2643b7b6237635c59d1ac9e
Brandon <80365319+Flaried@users.noreply.github.com>
2024-10-08 04:08:21 -0400

    add all the basic function on roblox and start testing

--- roblox_api.py
+++ roblox_api.py
@@ -1,26 +1,55 @@
 from handler import *
 import requests
 import datetime
-import rolimons_api
-
-
-
-def main():
-    rolimons_api.RolimonAPI().scan_owners()
-main()
+import os
+from rolimons_api import RolimonAPI
 
 class RobloxAPI():
     """
         Pass in Cookie if you want it to be an account
     """
 
-    def __init__(self, cookie:dict=None, Proxies=None) -> None:
-        self.request_handler = handle_requests.RequestsHandler(cookie=cookie)
+    def __init__(self, cookie:dict=None, Proxies=None):
+        self.request_handler = RequestsHandler(cookie=cookie)
         # no cookie
-        self.parse_handler = handle_requests.RequestsHandler(Proxies) 
+        self.parse_handler = RequestsHandler(Proxies) 
+        self.rolimon_parser = RolimonAPI()
 
-    def return_inventory(self, user_id):
-        userinventory = self.request_handler.get(f"https://inventory.roblox.com/v1/users/{userid}/assets/collectibles")
+        self.account_id = self.fetch_userid()
+        self.account_inventory = self.fetch_inventory(self.account_id, apply_NFT=True)
+        
+        if not self.account_inventory:
+            print(f"Account {self.account_id} has tradeable items")
+        print(self.check_can_trade(6419225))
 
+    def refresh_csrf(self):
+        token_post = self.request_handler.requestAPI('https://catalog.roblox.com/v1/catalog/items/details', method="post")
+
+        if 'x-csrf-token' in token_post.headers:
+            return token_post.headers['x-csrf-token']
+        else:
+            print("Couldnt fetch x-csrf-token")
+            return False
         pass
 
+    def fetch_userid(self):
+        auth_response = self.request_handler.requestAPI("https://users.roblox.com/v1/users/authenticated")
+        if auth_response.status_code == 200: 
+            return auth_response.json()['id']
+
+    def fetch_inventory(self, userid, apply_NFT=False):
+        return self.rolimon_parser.get_inventory(userid, apply_NFT)
+
+
+    def send_trade(self, trader_id, trade_send, trade_recieve):
+        pass
+
+    def check_can_trade(self, userid):
+        # TODO: Handle this pls tmr next priority
+        can_trade = self.request_handler.requestAPI(f"https://www.roblox.com/users/{userid}/trade")
+        if "NewLogin" in can_trade.url:
+            return False
+        return True
+#RobloxAccount = RobloxAPI().fetch_userid()
+
+print(RobloxAPI(cookie={'.ROBLOSECURITY': os.environ['cookie_secret']}).fetch_userid())
