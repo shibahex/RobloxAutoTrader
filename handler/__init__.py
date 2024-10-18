e4ff82475ce4188a8ea6a3e27536ac0157ee23ff
Brandon <80365319+Flaried@users.noreply.github.com>
2024-10-18 19:03:54 -0400

    add auth and json handlers

--- handler/__init__.py
+++ handler/__init__.py
@@ -2,4 +2,6 @@ from .handle_requests import RequestsHandler
 from .handle_config import ConfigHandler
 from .handle_discord import DiscordHandler
 from .handle_selenium import Chrome
-__all__ = ['RequestsHandler', 'ConfigHandler', 'Chrome', 'DiscordHandler'] 
+from .handle_json import JsonHandler
+from .handle_2fa import AuthHandler
+__all__ = ['RequestsHandler', 'ConfigHandler', 'Chrome', 'DiscordHandler', 'JsonHandler', 'AuthHandler'] 
