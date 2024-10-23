cd9be48449fc7578371bacbb3db81f9bc75bef0b
Brandon <80365319+Flaried@users.noreply.github.com>
2024-10-22 22:45:57 -0400

    Setup the main CLI and logic for the user

--- main.py
+++ main.py
@@ -1,59 +1,43 @@
+import rolimons_api
 from handler import *
 from roblox_api import RobloxAPI
-json_handler = JsonHandler('cookies.json')
-roblox_accounts = []
-cookie_warning = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_"
+from trade_algorithm import TradeMaker
+from account_manager import AccountManager
+import time
+
+class TradeManager:
+    def __init__(self):
+        pass
+
+class Doggo:
+    def __init__(self):
+        self.cli = Terminal()
+        pass
+
+    def main(self):
+        while True:
+            self.cli.clear_console()
+            options = (
+                (1, "Account Manager"),
+                (2, "Trade Manager"),
+                (3, "Execute Trader"),
+            )
+            self.cli.print_menu("Main Menu", options)
+            try:
+                answer = int(self.cli.input_prompt("Enter Option"))
+            except ValueError:
+                self.cli.print_error("Invalid input. Please enter a number.")
+                continue
+
+  #          print(answer)
+            match answer:
+                case 1:
+                    AccountManager().main()
+                case 2:
+                    pass
+ #           time.sleep(2)
 
 if __name__ == "__main__":
-    config = ConfigHandler('config.cfg')
-    while True:    
-        answer = int(input(
-            """
-    1. Add Cookie
-    2. Delete Cookie
-    3. Cancel All Trades
-    4. Scan inbound Trades
-    5. Scan Outbound Trades
-    7. Check for expired premium
-    8. Start Trade Bot
-    Enter Option:
-    """))
-        if answer == 1:
-            acc_cookie = input("Enter Cookie (include warning): ")
-            cookie_payload = {'.ROBLOSECURITY': acc_cookie}
+    doggo = Doggo()
+    doggo.main()
 
-            if cookie_warning not in acc_cookie:
-                print("Invalid cookie format try again")
-                continue
-
-            # CHECK IF COOKIE IN CORRECT
-            try:
-                roblox_login = RobloxAPI(cookie=cookie_payload)
-            except ValueError as e:
-                print(e)  # This will print the error message
-                print("Not using roblox account...")
-                continue
-
-            
-            auth_token = input("Enter in 2fa token: ")
-            try:
-                auth_code = roblox_login.verify_auth()
-            except ValueError as e:
-                print(e)
-                print("Not using account...")
-                continue
-
-            json_handler.add_cookie(acc_cookie, auth_token)
-
-            # CHECK IF USER HAS PREMIUM,
-            # TAKE USERNAME 
-            print("Cookie added successfully.")
-
-        elif answer == 2:
-            print("json")
-            json_handler.list_cookies()
-            index = int(input("Enter the number of the cookie to delete (or -1 to cancel): "))
-            if index != -1:
-                json_handler.delete_cookie(index)
-        elif answer == 3:
-            json_handler.list_cookies()
