6ab54ee1ab5d91fb2885f2ecd39125c46edb5f52
Brandon <80365319+Flaried@users.noreply.github.com>
2024-11-08 03:48:31 -0500

    Add roblox auth API logic

--- handler/handle_2fa.py
+++ handler/handle_2fa.py
@@ -1,21 +1,39 @@
 import pyotp
 import time
-
+import requests
+import json
 class AuthHandler:
     def __init__(self):
         # Get cookie and the auth for that cookie
         pass
 
-    def authenticate(self):
-        # return the auth code for that cookie
-        pass
+    def get_totp(self, auth_secret):
+        totp = pyotp.TOTP(auth_secret)
+        return totp.now()
 
-    def verify_auth_key(self, auth_secret):
-        try:
-            self.authenticator = pyotp.TOTP(auth_secret)
-            self.authenticator.now()
-            return self.authenticator
-        except:
-            print("Couldnt get auth")
-            raise ValueError("Authentication failed..")
 
+    def verify_request(self, req_handler, user_Id, metadata_challengeId, auth_code):
+        request = req_handler.Session.post("https://twostepverification.roblox.com/v1/users/" + user_Id + "/challenges/authenticator/verify", headers=req_handler.headers, json={
+            "actionType": "Generic",
+            "challengeId": metadata_challengeId,
+            "code": auth_code
+        })
+
+        if "errors" in request.json():
+            print("2fa error")
+            input(request.json()["errors"][0]["message"])
+            return False
+        return request.json()["verificationToken"]
+
+    def continue_request(self, req_handler, challengeId, verification_token, metadata_challengeId):
+        response = req_handler.Session.post("https://apis.roblox.com/challenge/v1/continue", headers=req_handler.headers, json={
+            "challengeId": challengeId,
+            "challengeMetadata": json.dumps({
+                "rememberDevice": True,
+                "actionType": "Generic",
+                "verificationToken": verification_token,
+                "challengeId": metadata_challengeId
+            }),
+            "challengeType": "twostepverification"
+        })
+        print(response.text, "AUTHNCTIACTION!!!")
