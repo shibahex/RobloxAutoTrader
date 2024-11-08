cea4291ba9776c8611cb0a311835e4514cdab39e
Brandon <80365319+Flaried@users.noreply.github.com>
2024-11-08 03:50:40 -0500

    fix 2fa validation and add projected checker

--- roblox_api.py
+++ roblox_api.py
@@ -1,84 +1,49 @@
 from handler import *
 import requests
 import datetime
+import time
 import os
 import pyotp
-from rolimons_api import RolimonAPI
+import json
+import base64
 
+import rolimons_api
+from handler.handle_json import JsonHandler
+from handler.handle_2fa import AuthHandler
 class RobloxAPI():
     """
         Pass in Cookie if you want it to be an account
     """
 
-    def __init__(self, cookie:dict=None, Proxies=None):
-        self.cookies = cookie
+    def __init__(self, cookie:dict=None, auth_secret=None, auth_ticket=None, Proxies=False):
 
-        self.request_handler = RequestsHandler(cookie=self.cookies)
-        # no cookie
-        self.parse_handler = RequestsHandler(Proxies) 
-        self.rolimon_parser = RolimonAPI()
-        
-        self.json_handler = JsonHandler('cookies.json')
+        self.parse_handler = RequestsHandler(Session=requests.Session(), use_proxies=True) 
+        self.config = ConfigHandler('config.cfg')
 
-        self.account_id, self.username = self.fetch_userid_and_name()
-        self.account_inventory = self.fetch_inventory(self.account_id, apply_NFT=True)
-        if self.account_id == False:
-            print("Failed to get userid for cookie", cookie)
-            raise ValueError("Invalid account or cookie.")
+        self.rolimon = rolimons_api.RolimonAPI()
 
-        if not self.account_inventory:
-            print(f"{self.username} has no tradeable items")
-            raise ValueError("Account has no tradeable items.")
+        if cookie != None:
+            self.cookies = cookie
+            self.authenticator = pyotp.TOTP(auth_secret)
+            self.auth_ticket = auth_ticket
+            self.request_handler = RequestsHandler(cookie=self.cookies, use_proxies=False)
+            self.json = JsonHandler('cookies.json')
+            self.auth_handler = AuthHandler()
 
-        self.request_handler.headers['x-csrf-token'] = self.refresh_csrf()
-        self.authenticator = None
-        #print(self.check_can_trade(6419225))
 
-    def verify_correct_auth(self):
-        json_file = self.json_handler.read_data()
-        account_list = json_file['roblox_accounts']
-        # find the account in cookies.json then fetch the auth token then return the Auth code
-        for account in account_list:
-            if account['cookie'] == self.cookies['.ROBLOSECURITY']:
-                try:
-                    self.authenticator = pyotp.TOTP(account['auth'])
-                    return self.authenticator
-                except:
-                    print("Couldnt get auth")
-                    raise ValueError("Authentication failed..")
+            self.account_id, self.username = self.fetch_userid_and_name()
+            self.account_inventory = self.fetch_inventory(self.account_id, apply_NFT=True)
+            if self.account_id == False:
+                print("Failed to get userid for cookie", cookie)
+                raise ValueError("Invalid account or cookie.")
+
+            if not self.account_inventory:
+                print(f"{self.username} has no tradeable items")
+                raise ValueError("Account has no tradeable items.")
+
+            self.request_handler.headers.update({'X-CSRF-TOKEN': self.refresh_csrf()})
 
     
-    def validate_authorization(self, challenge_id):
-        #if self.authenticator == None:
-            #self.authenticator = self.verify_correct_auth()
-        # NOTE: this is for testing purposes because i dont have multiple cookies setup yet
-        self.authenticator = pyotp.TOTP("X3N5BTXMDWKELMK3W5DNBUA3FA")
-
-        # Challenge ID is from a ticket from a bounded cookie
-        # To get a new token for a cookie you have to use the login API
-        # With selenium I should be able to get this token though.
-        # by 1. putting cookie in and 2. requesting to trade and then listening for an API
-        data = {"challengeId":"b07994e5-56e3-43c0-8ab4-e88a8b67c424","actionType":"Generic","code":f"{self.authenticator.now()}"}
-
-        response = self.request_handler.requestAPI("https://twostepverification.roblox.com/v1/users/1283171278/challenges/authenticator/verify", "post", payload=data)
-        
-        print(response.text)
-        print("now doing continue")
-        if response.status_code == 200:
-            verify_token = response.json()['verificationToken']
-
-
-            verify_payload = {
-                "challengeId": f"{challenge_id}",
-                "challengeMetadata": f'{{"verificationToken":"{verify_token}","rememberDevice":false,"challengeId":"{challenge_id}","actionType":"Generic"}}',
-                "challengeType": "twostepverification"
-            }
-
-            continue_api = self.request_handler.requestAPI("https://apis.roblox.com/challenge/v1/continue", "post", verify_payload)
-            print(continue_api.text, ("blablalfalfalfla"))
-            
-            return verify_token
-
     # refresh current inventory
     def refresh_self_inventory(self):
         self.account_inventory = self.fetch_inventory(self.account_id, apply_NFT=True)
@@ -90,7 +55,6 @@ class RobloxAPI():
 
         if 'x-csrf-token' in token_post.headers:
             print("returning",token_post.headers['x-csrf-token'])
-
             return token_post.headers['x-csrf-token']
         else:
             print("Couldnt fetch x-csrf-token")
@@ -105,10 +69,44 @@ class RobloxAPI():
             raise ValueError("Couldnt login with cookie", self.cookies)
 
     def fetch_inventory(self, userid, apply_NFT=False):
-        return self.rolimon_parser.get_inventory(userid, apply_NFT)
+        return self.rolimon.get_inventory(userid, apply_NFT)
 
     # NOTE: Payload:
     # {"offers":[{"userId":4486142832,"userAssetIds":[672469540],"robux":null},{"userId":1283171278,"userAssetIds":[1310053014],"robux":null}]}
+    def validate_2fa(self, response):
+        cookie_json = self.json.read_data()
+        
+        challengeid = response.headers["rblx-challenge-id"]
+        metadata = json.loads(base64.b64decode(response.headers["rblx-challenge-metadata"]))
+        try:
+            metadata_challengeid = metadata["challengeId"]
+        except Exception as e:
+            print("couldnt get meta data challengeid from", metadata, "scraping from", response.headers, "for meta data", response.url)
+            return False
+        try:
+            senderid = metadata["userId"]
+        except Exception as e:
+            print("couldnt get userid from", metadata, "scraping from", response.headers, "for meta data", response.url)
+            return False
+
+        # send the totp verify request to roblox
+        verification_token = self.auth_handler.verify_request(self.request_handler, senderid, metadata_challengeid, self.authenticator.now())
+
+        # send the continue request, its really important
+        self.auth_handler.continue_request(self.request_handler, challengeid, verification_token, metadata_challengeid)
+
+        # before sending the final payout request, add verification information to headers
+        return{
+            'rblx-challenge-id': challengeid,
+            'rblx-challenge-metadata': base64.b64encode(json.dumps({
+                "rememberdevice": True,
+                "actiontype": "generic",
+                "verificationtoken": verification_token,
+                "challengeid": metadata_challengeid
+            }).encode()).decode(),
+            'rblx-challenge-type': "twostepverification"
+        }
+
     def send_trade(self, trader_id, trade_send, trade_recieve):
         """
             Send Trader ID Then the list of items (list of assetids)
@@ -120,27 +118,33 @@ class RobloxAPI():
             "robux":None}]}
 
 
+        validation_headers = None
         while True:
-            trade_response = self.request_handler.requestAPI("https://trades.roblox.com/v1/trades/send", "post", payload=trade_payload)
+            trade_response = self.request_handler.requestAPI("https://trades.roblox.com/v1/trades/send", "post", payload=trade_payload, additional_headers=validation_headers)
             
             if trade_response.status_code == 200:
-                print("Trade sent!")
-                return True
+                print("Trade sent!", trade_response.text)
+                return trade_response.json()['id']
             elif trade_response.status_code == 403:
-                if "challenge" in trade_response.text.lower():
-                    print("solving 2fa..")
-                    challenge_ID = trade_response.headers['rblx-challenge-id']
-                    verify_token = self.validate_authorization(challenge_ID)
-                    # put verify token in headers I think
-                    # self.request_handler.headers['lna'] = verify_token
-                    break
+                print("403 error:", trade_response.json())
+                if 'rblx-challenge-id' in trade_response.headers:
+                    print("doing 2fa shit")
+                    validation = self.validate_2fa(trade_response)
+                    if validation == False:
+                        return None
+                    validation_headers = validation
+                    continue
+               
                 else:
+                    print("getting csrf")
                     newtoken = self.refresh_csrf()
                     if newtoken:
-                        self.request_handler.headers['x-csrf-token'] = self.refresh_csrf()
+                        self.request_handler.headers.update({'X-CSRF-TOKEN': newtoken})
+                        print("update x csrf token", self.request_handler.headers)
                     else: 
                         print("got error getting token")
                         break
+                    print("got 403 sending trade, trying to generate csrf")
             else:
                 print("errored at trade")
                 print(trade_response.text)
@@ -150,12 +154,125 @@ class RobloxAPI():
 
         pass
 
+    def outbound_checker(self):
+        pass
     def check_can_trade(self, userid):
         # TODO: Handle this pls tmr next priority
-        can_trade = self.request_handler.requestAPI(f"https://www.roblox.com/users/{userid}/trade")
+        validation_headers = None
+        can_trade = self.request_handler.requestAPI(f"https://www.roblox.com/users/{userid}/trade", additional_headers=validation_headers)
+        if can_trade.status_code == 403:
+            if "rblx-challenge-id" in can_trade.headers:
+                print(can_trade.headers, can_trade.text)
+                validation = self.validate_2fa(can_trade)
+                if validation == False:
+                    return None
+                validation_headers = validation
+
+            return False
         if "NewLogin" in can_trade.url:
             return False
         return True
-#RobloxAccount = RobloxAPI().fetch_userid()
+    
+    def is_projected(self, item_id):
+        # TODO: GET the dates and see how much the item sells so we dont trade dead items or we can
+        # see how active an item is
+        # TODO: Delete all the value: "1" from data points
+        """
+        Takes itemID and returns if its projected,
+        projected detection should have a cooldown so you dont spam the URL, 
+        so dont scan the same item over and over again, and dont scan value items
+        """
+        # Check if RAP - Price is correct min price difference
+        # TODO: scan detect rolimon projecteeds
+        rap = self.rolimon.item_data[item_id]['rap']
+        value = self.rolimon.item_data[item_id]['total_value']
+        price = self.rolimon.item_data[item_id]['best_price']
 
-#print(RobloxAPI(cookie={'.ROBLOSECURITY': os.environ['cookie_secret']}).fetch_userid_and_name()())
+        if value != rap:
+            return False
+
+        config_projected = self.config.projected_detection
+        min_graph_difference  = config_projected['MinimumGraphDifference']
+        max_graph_difference = config_projected['MaximumGraphDifference']
+        min_price_difference = config_projected['MinPriceDifference']
+
+        #TODO: ADD MIN AND MAX DIFFERENCE
+        #if not self.config.check_gain(int(rap), int(price), min_gain=min_price_difference, max_gain=max_price_difference):
+        #    print("projected due to price difference")
+        #    return True
+
+        # Check graph for projecteds
+        resale_data = self.parse_handler.requestAPI(f"https://economy.roblox.com/v1/assets/{item_id}/resale-data")
+        if resale_data.status_code == 200:
+            #print(resale_data.json())
+            data_points = resale_data.json()['priceDataPoints']
+
+            for data in data_points:
+                if data['value'] == 1:
+                    data_points.remove(data)
+            # If doesnt have enough sale data just mark it as projected
+            if len(data_points) < 100:
+                return True
+
+            current_data = int(data_points[0]['value'])
+
+            amount_of_sales = config_projected['AmountofSalestoScan']
+
+            #print(data_points, current_data)
+            for data_point in range(1, amount_of_sales+1):
+                rap_data_point = int(data_points[data_point]['value'])
+                is_in_range = self.config.check_gain(rap_data_point, current_data, min_gain=min_graph_difference, max_gain=max_graph_difference) 
+                #print(rap_data_point - current_data,  ": minimum", min_graph_difference, "max", max_graph_difference, is_in_range)
+
+                if is_in_range == False:
+                    return True
+            
+            #for volume_data in resale_data.json()['volumeDataPoints']:
+            volume_data_points = resale_data.json().get('volumeDataPoints', [])[:30]
+            if self.analyze_volume_data(volume_data_points) == False:
+                return True
+
+
+        return False
+
+    def analyze_volume_data(self, volume_data_points):
+        # Function to calculate date differences in days
+        def calculate_date_diff(dates):
+            date_diffs = []
+            for i in range(1, len(dates)):
+                # Parse the previous and current date string to datetime objects
+                prev_date = datetime.datetime.strptime(dates[i - 1], "%Y-%m-%dT%H:%M:%SZ")
+                curr_date = datetime.datetime.strptime(dates[i], "%Y-%m-%dT%H:%M:%SZ")
+                # Calculate the difference in days between the two dates
+                diff = abs((curr_date - prev_date).days)
+                date_diffs.append(diff)
+            return date_diffs
+        
+        values = [point['value'] for point in volume_data_points]
+        dates = [point['date'] for point in volume_data_points]
+
+        avg_value = sum(values) / len(values) if values else 0
+        print(f"Average Value: {avg_value:.2f}")
+
+        date_diffs = calculate_date_diff(dates)
+
+        avg_date_diff = sum(date_diffs) / len(date_diffs) if date_diffs else 0
+        print(f"Average Date Difference (in days): {avg_date_diff:.2f}")
+
+        large_gaps = [diff for diff in date_diffs if diff > 7]
+
+        # Optionally, print the large gaps for debugging purposes
+        if large_gaps:
+            print("Large gaps between dates (in days):", large_gaps)
+            return False
+
+#while True:
+#    hat = input("enter id > ")
+#    if RobloxAPI().is_projected(hat) == True:
+#        print("Is projected")
+#    else:
+#        print("Not projected")
+    
+
+
+
