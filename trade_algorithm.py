9ded7c458f3a56d85002d81e8e6972551a5adc82
Brandon <80365319+Flaried@users.noreply.github.com>
2024-11-08 03:52:07 -0500

    add min and max sides to support upgrading/downgrading

--- trade_algorithm.py
+++ trade_algorithm.py
@@ -15,48 +15,75 @@ class TradeMaker():
         self.min_score_percentage = self.config.trading['MinScorePercentage']
         self.max_score_percentage = self.config.trading['MaxScorePercentage']
 
+        self.min_items_self = self.config.trading['MinimumItemsYourSide']
+        self.max_items_self = self.config.trading['MaximumItemsTheirSide']
+
+        self.min_items_their = self.config.trading['MinimumItemsTheirSide']
+        self.max_items_their = self.config.trading['MaximumItemsTheirSide']
+
+
 
 
     def generate_trade(self, self_inventory, their_inventory):
-        #print("Got inventories", self_inventory, their_inventory)
-        # get all useful rolimon data and apply to inventories
-
         self_keys = list(self_inventory.keys())
         their_keys = list(their_inventory.keys())
 
-        # all combinations of up to 4 items on each side
-        self_combinations = self.generate_combinations(self_keys)
-        their_combinations = self.generate_combinations(their_keys)
+        # Generate combinations for both inventories
+        self_combinations = self.generate_combinations(self_keys, self.min_items_self, self.max_items_self)
+        their_combinations = self.generate_combinations(their_keys, self.min_items_their, self.max_items_their)
 
-
-        # Debug: Print possible trades
+        # Create sets of item IDs for quick lookup
+        self_item_ids = {self_inventory[key]['item_id'] for key in self_keys}
         valid_trades = []
-        for trade in product(self_combinations, their_combinations):
-            self_side, their_side = trade
-            if self.validate_trade(self_side, their_side, self_inventory, their_inventory):
-                valid_trades.append(trade)
-        if len(valid_trades) > 1:
-            return valid_trades[0]
-        else: 
-            return None
+
+        for self_side in self_combinations:
+            # Create a set for the current self_side to check against
+            self_side_item_ids = {self_inventory[key]['item_id'] for key in self_side}
+
+            for their_side in their_combinations:
+                their_side_item_ids = {their_inventory[key]['item_id'] for key in their_side}
+
+                # Ensure no overlapping item IDs
+                if self_side_item_ids.isdisjoint(their_side_item_ids):
+                    if self.validate_trade(self_side, their_side, self_inventory, their_inventory):
+                        valid_trades.append((self_side, their_side))
+                        if len(valid_trades) > 1:
+                            return valid_trades[0]
+
+        return valid_trades[0] if valid_trades else None
 
 
-        #return possible_trades
+    def check_rap_gain(self, their_rap, self_rap):
+        return self.config.check_gain(their_rap, self_rap, self.min_rap_gain, self.max_rap_gain)
 
-    def validate_trade(self,  self_side, their_side, self_inventory, their_inventory):
-        self_value = sum(self_inventory[key]['value'] for key in self_side)
-        self_rap = sum(self_inventory[key]['rap'] for key in self_side)
+    def check_value_gain(self, their_value, self_value):
+        return self.config.check_gain(their_value, self_value, self.min_value_gain, self.max_value_gain)
 
-        their_value = sum(their_inventory[key]['value'] for key in their_side)
-        their_rap = sum(their_inventory[key]['value'] for key in their_side)
 
-        #(Sum_of_Rap(theiritem) - Sum_of_Rap(youritem)) / (Sum_of_Rap(theiritem) + Sum_of_Rap(youritem))
+    def validate_trade(self, self_side, their_side, self_inventory, their_inventory):
+        # Precompute the total value and RAP for both sides in a single loop
+        self_value = 0
+        self_rap = 0
+        for key in self_side:
+            item = self_inventory[key]
+            self_value += item['value']
+            self_rap += item['rap']
 
+        their_value = 0
+        their_rap = 0
+        for key in their_side:
+            item = their_inventory[key]
+            their_value += item['value']
+            their_rap += item['rap']
+
+        # Calculate value gain and close percentage
         value_gain = their_value - self_value
+        if self_rap + their_rap == 0:
+            close_percentage = 0  # Prevent division by zero
+        else:
+            close_percentage = ((their_rap - self_rap) / (their_rap + self_rap)) * 100
 
-        close_percentage = ((their_rap - self_rap) / (their_rap + self_rap))*100
-
-
+        # Check if close percentage is within the acceptable range
         if close_percentage < self.min_score_percentage or close_percentage > self.max_score_percentage:
             return False
 
@@ -64,13 +91,11 @@ class TradeMaker():
         if not self.check_rap_gain(their_rap, self_rap):
             return False
 
+        # Check if value gain passes the criteria
         if not self.check_value_gain(their_value, self_value):
             return False
 
-        #print(self_value, self_rap, "for", their_value, their_rap)#, "value and rap gain:", value_gain, rap_gain, close_percentage*100)#,
-
         return True
-
     def check_rap_gain(self, their_rap, self_rap):
         # Calculate the RAP gain
         rap_gain = their_rap - self_rap
@@ -101,10 +126,9 @@ class TradeMaker():
             # If it's not a float, compare the value gain as an absolute value
             return value_gain >= self.min_value_gain and value_gain <= self.max_value_gain
 
-    def generate_combinations(self, keys):
-        # Generate all combinations of up to max_len items
-        max_items = 4
+
+    def generate_combinations(self, keys, min_items=1, max_items=4):
         all_combinations = []
-        for r in range(1, max_items + 1):
+        for r in range(min_items, max_items + 1):
             all_combinations.extend(combinations(keys, r))
         return all_combinations
