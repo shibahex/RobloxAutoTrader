0d45e540a248f8e86516395807f0748194b82fc8
Brandon <80365319+Flaried@users.noreply.github.com>
2024-11-08 03:47:19 -0500

    add support for user to login through selenium

--- account_manager.py
+++ account_manager.py
@@ -37,8 +37,11 @@ class AccountManager:
             self.json_handler.list_cookies()
             try:
                 index = self.cli.input_prompt("Enter the number of the cookie to delete (Press enter to stop)")
-                self.json_handler.delete_cookie(index)
-            except:
+                self.json_handler.delete_cookie(int(index))
+            except ValueError:
+                self.cli.print_error(f"Invalid input: '{index}' is not a valid number.")
+            except Exception as e:
+                self.cli.print_error(f"got execption {e} trying to delete cookie")
                 break
 
     def manually_add_account(self):
@@ -47,12 +50,10 @@ class AccountManager:
         try:
             auth_code = AuthHandler().verify_auth_key(auth_secret)
         except ValueError as e:
-            print(e)
-            self.cli.print_error("Not using account...")
+            self.cli.print_error(f"{e}\nSkipping account...")
             return None 
 
         acc_cookie = self.cli.input_prompt("Enter Cookie (include warning)")
-        cookie_payload = {'.ROBLOSECURITY': acc_cookie}
 
         if cookie_warning not in acc_cookie:
             self.cli.print_error("Invalid cookie format try again")
@@ -61,29 +62,29 @@ class AccountManager:
         try:
             roblox_login = RobloxAPI(cookie=cookie_payload)
         except ValueError as e:
-            print(e)  # This will print the error message
-            self.cli.print_error("Not using roblox account...")
+            self.cli.print_error(f"{e}\nSkipping account...")
             return None
 
         auth_ticket = self.cli.input_prompt("enter auth ticket")
+        self.json_handler.add_cookie(acc_cookie, auth_secret, auth_ticket)
+
 
     def add_account(self):
-        # TODO: dont ask for auth_secret if its in cookies.json somehow
         auth_secret = self.cli.input_prompt("Enter the authorization key")
 
+        firefox = FirefoxLogin()
         try:
             auth_code = AuthHandler().verify_auth_key(auth_secret)
         except ValueError as e:
-            print(e)
-            self.cli.print_error("Not using account...")
+            self.cli.print_error(f"{e}\nSkipping account...")
             return None 
         try:
-            cookie, ticket = FirefoxLogin().roblox_login(auth_secret)
+            cookie, auth_ticket = firefox.roblox_login(auth_secret)
+            firefox.stop()
         except ValueError as e:
-            print(e)
-            self.cli.print_error("Not using account..")
+            self.cli.print_error(f"{e}\nSkipping account...")
+            firefox.stop()
             return None
-        # TODO: PUT THIS IN cookies.json
-        print(cookie, ticket)
+        self.json_handler.add_cookie(cookie, auth_secret, auth_ticket)
 
 
