c0614d3d8b0ef316ba8221cec28c6a76b31fb228
Brandon <80365319+Flaried@users.noreply.github.com>
2024-12-02 18:27:47 -0500

    remove useless functions

--- trade_algorithm.py
+++ trade_algorithm.py
@@ -5,14 +5,15 @@ import math
 
 import time
 class TradeMaker():
-    def __init__(self):
-        self.config = ConfigHandler('config.cfg')
+    def __init__(self, config=ConfigHandler('config.cfg')):
+        self.config = config
 
         self.min_rap_gain = self.config.trading['Minimum_RAP_Gain']
         self.max_rap_gain = self.config.trading['Maximum_RAP_Gain']
         self.min_algo_gain = self.config.trading['Minimum_Algo_Gain']
         self.max_algo_gain = self.config.trading['Maximum_Algo_Gain']
 
+        print(self.min_rap_gain, self.max_rap_gain, "hay")
         self.min_value_gain = self.config.trading['Minimum_Value_Gain']
         self.max_value_gain = self.config.trading['Maximum_Value_Gain']
         self.min_score_percentage = self.config.trading['MinScorePercentage']
@@ -275,37 +276,6 @@ class TradeMaker():
             return False
 
         return True
-
-    def check_rap_gain(self, their_rap, self_rap):
-        # Calculate the RAP gain
-        rap_gain = their_rap - self_rap
-
-        # Check if the minimum RAP gain is a float (i.e., percentage-based)
-        if not self.min_rap_gain.is_integer():
-            percentage_rap_gain = ((their_rap - self_rap) / their_rap) * 100 if their_rap != 0 else 0
-            min_rap_gain = self.min_rap_gain * 100
-            max_rap_gain = self.max_rap_gain * 100
-
-            return min_rap_gain <= percentage_rap_gain <= max_rap_gain
-        else:
-            # If it's not a float, compare the RAP gain as an absolute value
-            return rap_gain >= self.min_rap_gain and rap_gain <= self.max_rap_gain
-
-    def check_value_gain(self, their_value, self_value):
-        # Calculate the value gain
-        value_gain = their_value - self_value
-
-        # Check if the minimum value gain is a float (i.e., percentage-based)
-        if not self.min_value_gain.is_integer():
-            percentage_value_gain = ((their_value - self_value) / their_value) * 100 if their_value != 0 else 0
-            min_value_gain = self.min_value_gain * 100
-            max_value_gain = self.max_value_gain * 100
-
-            return min_value_gain <= percentage_value_gain <= max_value_gain
-        else:
-            # If it's not a float, compare the value gain as an absolute value
-            return value_gain >= self.min_value_gain and value_gain <= self.max_value_gain
-
 
     def generate_combinations(self, keys, min_items=1, max_items=4):
         all_combinations = []
