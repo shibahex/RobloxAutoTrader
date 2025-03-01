4786b741457890aa5aec29b64a14aef08b03fe88
Brandon <badamsva@gmail.com>
2025-03-01 01:04:41 -0500

    Always write to .whitelist so users dont have to repeat login info

--- whitelist_manager.py
+++ whitelist_manager.py
@@ -3,6 +3,9 @@ from handler.handle_json import JsonHandler
 from handler.handle_cli import Terminal
 import handler.handle_cli
 import time
+import traceback
+import os
+import ctypes
 class WhitelistManager:
     def __init__(self):
         self.cli = Terminal()
@@ -10,16 +13,16 @@ class WhitelistManager:
         self.json = JsonHandler(".whitelist")
 
     def get_info(self):
-        order_id = str(input("Enter OrderID: "))
+        order_id = str(input("Enter License Key: "))
         username = str(input("Enter Username: "))
         password = str(input("Enter Password: "))
+        self.json.write_data({'username': username, 'password': password, 'orderid': order_id})
         return order_id, username, password
 
     def register_user(self):
         order_id, username, password = self.get_info()
         registered = self.Whitelist.register_user(username, password, order_id)
         if registered == True:
-            self.json.write_data({'username': username, 'password': password, 'orderid': order_id})
             print("User sucessfully registered")
         else:
             print("User couldn't get registered..")
@@ -44,14 +47,16 @@ class WhitelistManager:
             try:
                 match answer:
                     case 1:
+                        print("Please create login details.")
                         self.register_user()
                     case 2:
                         order_id, username, password = self.get_info()
                         valid = self.Whitelist.is_valid(username, password, order_id)
                         if valid:
-                            self.json.write_data({'username': username, 'password': password, 'orderid': order_id})
+                            print("Valid Whitelist!")
                         else:
                             print("Try reseting the HWID and IP")
+                            time.sleep(1)
                     case 3:
                         data = self.json.read_data()
                         if data and data.get('username') and data.get('password') and data.get('orderid'):
@@ -60,15 +65,18 @@ class WhitelistManager:
                             order_id = data.get('orderid')
                         else:
                             order_id, username, password = self.get_info()
-                        sucess = self.Whitelist.reset_ip(data['username'], data['password'], data['orderid'])
+
+                        sucess = self.Whitelist.reset_ip(username, password, order_id)
                         if sucess:
                             print("Sucesfully reset IP, please wait 90 minutes before reseting again")
                             time.sleep(2)
                         else:
-                            print("Couldn't reset IP, whitelist invalid, delete .whitelist and try again, or wait 90 minutes")
-                            time.sleep(3)
+                            print("Couldn't reset IP, whitelist invalid, try to login existing order and try again, or wait 90 minutes for whitelist restart")
+                            time.sleep(4)
                     case 4:
                         break
             except Exception as e:
+                tb = traceback.format_exc()  # Capture the full traceback
+                print(e, tb)
                 self.cli.print_error(e)
                 continue
